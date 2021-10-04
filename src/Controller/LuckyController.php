86ddf3a57be915fd905ab28d2deef7075e9bd2fe
Ryan Weaver <ryan@thatsquality.com>
2021-10-04 13:08:57 -0400

    [tuts-apply-diff] Applying existing diff file for step basics-max-route-wildcard

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -6,9 +6,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController
 {
-    public function number(): Response
+    public function number($max): Response
     {
-        $number = random_int(0, 100);
+        $number = random_int(0, $max);
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
