fdbe0a6af1b3f27033c48d299f4624150cf1cbba
Ryan Weaver <ryan@thatsquality.com>
2021-10-06 15:09:07 -0400

    tuts hero working...

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,9 +18,19 @@ class ProductController extends AbstractController
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
         $results = $pdo->query('SELECT * FROM product')->fetchAll();
+        $products = [];
+        foreach ($results as $result) {
+            $product = new Product();
+            $product->setId($result['id']);
+            $product->setName($result['name']);
+            $product->setPrice($result['price']);
+            $product->setDescription($result['description']);
+
+            $products[] = $product;
+        }
 
         return $this->render('product/list.html.twig', [
-            'products' => $results,
+            'products' => $products,
         ]);
     }
 }
